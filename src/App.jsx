4bef169dc8bca5e9b393e3f80437af0503fdc008
Sahil Chouhan <chouhangolu604@gmail.com>
2026-04-20 07:36:16 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 
+import AutocompleteComponent from './components/AutocompleteComponent'
+import Buttons from './components/Buttons'
+import UploadFile from './components/UploadFile'
+
 function App() {
   
 
   return (
     <>
-      
+      <Buttons />
+      <UploadFile />
+      <AutocompleteComponent />
     </>
   )
 }
